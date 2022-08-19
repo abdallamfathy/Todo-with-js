3ef44a61b5d90e0bb6cd1edea6c74287fa27948d
abdallamfathy <abdallamfathy@gmail.com>
2022-08-19 15:22:47 +0200

    setting variables

---
+++ main.js
@@ -0,0 +1,9 @@
+// Setting variables
+let container = document.querySelector(".todo-container");
+let input = document.querySelector(".add-task input");
+let addButton = document.querySelector(".add-task span");
+let noTaskMessage = document.querySelector(".tasks-content span");
+
+
+
+
